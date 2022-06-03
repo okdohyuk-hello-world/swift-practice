735f3531936a415b5525f7d57331eaf913ddf911
okdohyuk <okdohyuk@gmail.com>
2022-06-04 02:58:35 +0900

    Initial Commit

---
+++ swift-practice/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  swift-practice
+//
+//  Created by 유도혁 on 2022/06/04.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ swift-practice/swift_practiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  swift_practiceApp.swift
+//  swift-practice
+//
+//  Created by 유도혁 on 2022/06/04.
+//
+
+import SwiftUI
+
+@main
+struct swift_practiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
