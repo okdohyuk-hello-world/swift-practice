eb78f629f63cca1f285958d6556bc56223ac0847
okdohyuk <okdohyuk@gmail.com>
2022-06-04 18:19:15 +0900

    feat todo on change save to device

--- swift-practice/ContentView.swift
+++ swift-practice/ContentView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     //    Identifiable = 식별가능한 아이템?
-    struct TodoList: Identifiable {
+    struct TodoList: Identifiable, Codable {
         let id = UUID()
         var title: String
         var content: String
@@ -17,7 +17,11 @@ struct ContentView: View {
     }
     
     @State private var todoString = ""
-    @State private var todoLists = [TodoList]()
+    @State private var todoLists = [TodoList]() {
+        didSet {
+            saveTodoList()
+        }
+    }
     
     func addNewTodo() {
         let newTodo = TodoList(title: todoString, content: "", isChecked: false)
@@ -31,6 +35,82 @@ struct ContentView: View {
     
     func removeTodo(_ idx: Int) {
         todoLists.remove(at: idx)
+    }
+    
+    func generateToJSON() -> String? {
+        let encoder = JSONEncoder()
+        encoder.outputFormatting = .prettyPrinted
+        
+        do {
+            let data = try encoder.encode(todoLists)
+            return String(data: data, encoding: .utf8)
+        }
+        catch {
+            print(error.localizedDescription)
+        }
+        
+        return nil
+    }
+    
+    func generateToData(_ jsonString: String) -> [TodoList]? {
+        let decoder = JSONDecoder()
+        let jsonData = jsonString.data(using: .utf8)
+        
+        if jsonData == nil {
+            print("Error: Cannot convert json String to Data")
+            return nil
+        }
+        
+        do {
+            return try decoder.decode([TodoList].self, from: jsonData!)
+        }
+        catch {
+            print(error.localizedDescription)
+        }
+        
+        return nil
+    }
+    
+    func getDocumentPath() -> URL {
+        return FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)[0]
+    }
+    
+    func saveTodoList() {
+        let path = getDocumentPath().appendingPathComponent("todolist.json")
+        let jsonString = generateToJSON()
+        
+        if jsonString == nil {
+            print("Error: No JSON String found")
+            return
+        }
+        
+        print(path.absoluteString)
+        
+        do {
+            try jsonString?.write(to: path, atomically: true, encoding: .utf8)
+        }
+        catch {
+            print(error.localizedDescription)
+        }
+    }
+    
+    func loadTodoList() {
+        let path = getDocumentPath().appendingPathComponent("todolist.json")
+        
+        do {
+            let jsonString = try String(contentsOf: path)
+            let jsonData = generateToData(jsonString)
+            
+            if jsonData == nil {
+                print("Error: No JSON Data found")
+                return
+            }
+            
+            todoLists = jsonData!
+        }
+        catch {
+            print(error.localizedDescription)
+        }
     }
     
     var body: some View {
@@ -59,9 +139,12 @@ struct ContentView: View {
                             Image(systemName: "trash")
                         })
                     }.buttonStyle(BorderlessButtonStyle())
-                    
                 }
+                
             }
+        }
+        .task {
+            loadTodoList()
         }
     }
 }
@@ -69,7 +152,6 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
-            .previewDevice("iPhone 13 mini")
     }
 }
 
